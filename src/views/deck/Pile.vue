a8e485c9dec8ab53459e355d12ff9ee3561a2d96
Webstar921 <webstar0921@gmail.com>
2020-03-30 19:46:09 +0200

    design Ordered Pile page based on states from vuex store

--- src/views/deck/Pile.vue
+++ src/views/deck/Pile.vue
@@ -1,12 +1,88 @@
 <template>
-  <div class="pile">
-    Pile view skeleton
+  <div v-if="pileLoaded" class="pile">
+    <div class="pile-content container row">
+      <div 
+        v-for="(card, index) in cards"
+        class="pile-content-item col-lg-3"
+        :key="index"
+      >
+        <card-view
+          :value="card.value"
+          :suit="card.suit"
+        />
+      </div>
+    </div>
+
+    <div class="pile-footer">
+      <div class="pile-footer-info">
+        <footer-info-item
+          :title="'Rotation Card'"
+          :info="rotationCard"
+        />
+        <footer-info-item
+          :title="'High Card'"
+          :info="highCard"
+        />
+        <footer-info-item
+          class="full-house"
+          :title="'Full House Combo'"
+          :info="fullHouseStatus"
+          :fullHouseCombinations="fullHouse"
+        />
+      </div>
+    </div>
+  </div>
+  <div v-else class="loading-wrapper">
+    <b-spinner variant="success" label="Spinning"></b-spinner>
   </div>
 </template>
 <script>
+import CardView from '@components/CardView'
+import FooterInfoItem from '@components/FooterInfoItem'
 
 export default {
-  name: 'Pile'
+  name: 'Pile',
+
+  components: {
+    CardView,
+    FooterInfoItem
+  },
+
+  data () {
+    return {
+      rotationCode: '',
+      highCard: '',
+      fullHouseStatus: '',
+      fullHouse: [],
+      cards: []
+    }
+  },
+
+  watch: {
+    orderedCards(val) {
+      // set cards data
+      this.cards = val
+      console.log('val in pile view: ', val)
+
+      // set highCard data
+      if (val.length > 0) {
+        this.highCard = val[0].code
+      } else {
+        this.highCard = 'None'
+      }
+    }
+  },
+
+  mounted () {
+    const {
+      rotationCard,
+      pileName,
+      pileAdded,
+      $route: {
+        params: { id }
+      }
+    } = this
+  }
 }
 </script>
 
