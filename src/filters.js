6476ba0d9d6506a62b3c6fd374c2da60d4fb6cc4
Webstar921 <webstar0921@gmail.com>
2020-03-30 21:36:14 +0200

    filter function and image assets

--- src/filters.js
+++ src/filters.js
@@ -1,4 +1,5 @@
 import dayjs from 'dayjs'
+import { romanObj } from '@helper/constants'
 
 export default {
   dateTimeConvert (time) {
@@ -6,5 +7,36 @@ export default {
   },
   dateConvert (time) {
     return time && dayjs(time).format('YYYY-MM-DD')
+  },
+
+  getImageUrl(imageName) {
+    const images = require.context('./assets/images', false, /\.png$/)
+    return images('./' + imageName + '.png')
+  },
+
+  convertToRoman(num) {
+    const arrConv = romanObj
+
+    let arr = num.toString().split('').reverse()
+    let i = 1
+    for (let k = 0; k < arr.length; k++) {
+      arr.splice(k, 1, arr[k] * i)
+      i *= 10
+    }
+
+    let romansArray = []
+    for (i = 0; i < arr.length; i++) {
+      romansArray.push(arrConv[arr[i]] || '')
+    }
+
+    // It returns the string with the roman number
+    return romansArray.reverse().join('')
+  },
+
+  filterValue(value) {
+    if (value.length > 2) {
+      return value.substr(0, 1)
+    }
+    return value
   }
 }
--- src/helper/constants.js
+++ src/helper/constants.js
@@ -4,3 +4,4 @@ export const API_URL = 'https://deckofcardsapi.com/api'
 export const TOTAL_CARD = 52
 export const INITIAL_SUITS = ['H', 'D', 'C', 'S']
 export const INITIAL_VALUES = ['2', 'A', 'K', 'Q', 'J', '0', '9', '8', '7', '6', '5', '4', '3']
+export const romanObj = { 1: 'I', 2: 'II', 3: 'III', 4: 'IV', 5: 'V', 6: 'VI', 7: 'VII', 8: 'VIII', 9: 'IX', 10: 'X', 20: 'XX', 30: 'XXX', 40: 'XL', 50: 'L', 60: 'LX', 70: 'LXX', 80: 'LXXX', 90: 'XC', 100: 'C', 200: 'CC', 300: 'CCC', 400: 'CD', 500: 'D', 600: 'DC', 700: 'DCC', 800: 'DCCC', 900: 'CM', 1000: 'M', 2000: 'MM', 3000: 'MMM', 4000: 'MMMM', 5000: 'MMMMM', 6000: 'MMMMMM', 7000: 'MMMMMMM', 8000: 'MMMMMMMM', 9000: 'MMMMMMMMM' }
