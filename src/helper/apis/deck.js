b44879b23235a9a836264159d9a421e6b9617588
Webstar921 <webstar0921@gmail.com>
2020-03-30 19:26:19 +0200

    api for creating new deck and adding cards to pile

---
+++ src/helper/apis/deck.js
@@ -0,0 +1,20 @@
+import $ajax from '../ajax'
+import * as c from '../constants'
+
+const apiUrl = c.API_URL
+const totalCard = c.TOTAL_CARD
+
+export default {
+  addNewDeck () {
+    return $ajax.get(`${apiUrl}/deck/new/`)
+  },
+
+  drawCard (deckId) {
+    return $ajax.get(`${apiUrl}/deck/${deckId}/draw/?count=${totalCard}`)
+  },
+
+  addToPile (deckId, cardValues) {
+    const query = cardValues.join()
+    return $ajax.get(`${apiUrl}/deck/${deckId}/pile/pile_${deckId}/add/?cards=${query}`)
+  }
+}
--- src/helper/constants.js
+++ src/helper/constants.js
@@ -1,2 +1,4 @@
 export const FIRST_QUEUE = [0, 1, 2, 3, 4]
 export const SECOND_QUEUE = [5, 6, 7, 8, 9]
+export const API_URL = 'https://deckofcardsapi.com/api'
+export const TOTAL_CARD = 52
