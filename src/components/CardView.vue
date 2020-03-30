470ed82de4372178efa338707d5eb73d08637d9d
Webstar921 <webstar0921@gmail.com>
2020-03-30 20:11:19 +0200

    create CardView and FooterInfoItem skeleton components

---
+++ src/components/CardView.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="card-view">
+    Card View Skeleton
+    //TODO: card view content
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CardInput',
+  props: {
+    value: Number,
+    suit: String
+  },
+  data () {
+    return {
+      value: '',
+      suit: 'spade'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.card-view {
+  position: relative;
+  display: flex;
+  width: 136px;
+  height: 238px;
+  border-radius: 10px;
+  box-shadow: 3px 3px 10px 0 #826693;
+  background-color: #ffffff;
+  font-family: Arial;
+}
+</style>
+
+
---
+++ src/components/FooterInfoItem.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="footer-info-item">
+    Footer Info Skeleton
+    //TODO: Footer Info content
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FooterInfoItem',
+  props: {
+    title: {
+      type: String,
+      default: ''
+    },
+    info: {
+      type: String,
+      default: ''
+    },
+    fullHouseCombinations: {
+      type: Array,
+      default: []
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.footer-info-item {
+  display: flex;
+  padding: 10px 0;
+  font-family: Arial;
+}
+</style>
